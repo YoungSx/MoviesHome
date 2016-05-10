eb2aa12a53e1a55aaea5e36e0023b5e26f24462d
Shangxin <shangxin@outlook.com>
2016-05-10 13:23:46 +0800

    test

--- src/link/shangxin/movies/MainActivity.java
+++ src/link/shangxin/movies/MainActivity.java
@@ -1,24 +1,33 @@
 package link.shangxin.movies;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	Button searchButton;
+	EditText searchBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        searchButton = (Button) findViewById(R.id.searchButton);
+        searchBox = (EditText) findViewById(R.id.searchBox);
+        String searchText = searchBox.getText().toString();
         
+        searchButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//Intent intent = new Intent(MainActivity.this,);
+			}
+		});
         
     }
 
