830040c59c3a162cc8bba0df46bcfab7ddf890aa
liuhaoran <763902175@qq.com>
2016-05-11 08:59:10 +0800

    结果页样式2

--- gen/link/shangxin/movies/R.java
+++ gen/link/shangxin/movies/R.java
@@ -22,12 +22,22 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int beijing=0x7f020000;
+        public static final int beijing2=0x7f020001;
+        public static final int bg=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080009;
+        public static final int daoyan=0x7f080005;
+        public static final int dianyingming=0x7f080003;
+        public static final int dianyingzhijia=0x7f080002;
+        public static final int juqing=0x7f080008;
+        public static final int leixing=0x7f080004;
         public static final int searchBox=0x7f080000;
         public static final int searchButton=0x7f080001;
+        public static final int shangyingshijian=0x7f080007;
+        public static final int zhuyan=0x7f080006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
