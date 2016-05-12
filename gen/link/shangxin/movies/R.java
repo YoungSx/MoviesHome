b64eed52542079a2e5686125f7e9a9cdb7b476ee
Shangxin <shangxin@outlook.com>
2016-05-12 14:16:39 +0800

    welcome

--- gen/link/shangxin/movies/R.java
+++ gen/link/shangxin/movies/R.java
@@ -29,24 +29,25 @@ public final class R {
         public static final int timg=0x7f020004;
     }
     public static final class id {
-        public static final int action_settings=0x7f080018;
+        public static final int action_settings=0x7f080019;
+        public static final int container=0x7f08000d;
         public static final int daoyan=0x7f080008;
         public static final int dianyingming=0x7f080006;
         public static final int hotList=0x7f080002;
-        public static final int hotName=0x7f08000e;
-        public static final int hotTboxoffice=0x7f080011;
-        public static final int hotWboxoffice=0x7f080010;
-        public static final int hotWk=0x7f08000f;
+        public static final int hotName=0x7f08000f;
+        public static final int hotTboxoffice=0x7f080012;
+        public static final int hotWboxoffice=0x7f080011;
+        public static final int hotWk=0x7f080010;
         public static final int juqing=0x7f08000c;
         public static final int leixing=0x7f080007;
-        public static final int myLayout=0x7f08000d;
+        public static final int myLayout=0x7f08000e;
         public static final int pingfen=0x7f08000a;
-        public static final int recentDirector=0x7f080015;
-        public static final int recentPlayDate=0x7f080013;
-        public static final int recentStar=0x7f080014;
-        public static final int recentStory=0x7f080017;
-        public static final int recentTvTitle=0x7f080012;
-        public static final int recentType=0x7f080016;
+        public static final int recentDirector=0x7f080016;
+        public static final int recentPlayDate=0x7f080014;
+        public static final int recentStar=0x7f080015;
+        public static final int recentStory=0x7f080018;
+        public static final int recentTvTitle=0x7f080013;
+        public static final int recentType=0x7f080017;
         public static final int rmButton=0x7f080003;
         public static final int searchBox=0x7f080000;
         public static final int searchButton=0x7f080001;
@@ -58,19 +59,22 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_result=0x7f030001;
-        public static final int hotitem=0x7f030002;
-        public static final int recentitem=0x7f030003;
+        public static final int activity_welcome=0x7f030002;
+        public static final int hotitem=0x7f030003;
+        public static final int recentitem=0x7f030004;
     }
     public static final class menu {
         public static final int jiemian=0x7f070000;
         public static final int main=0x7f070001;
         public static final int result=0x7f070002;
+        public static final int welcome=0x7f070003;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
         public static final int title_activity_result=0x7f050003;
+        public static final int title_activity_welcome=0x7f050004;
     }
     public static final class style {
         /** 
