7f64392a7bb5cc748efc5d0304a1ef0f76d9ff4a
wanghu <741106631@qq.com>
2016-05-12 17:00:28 +0800

    修复分辨率适配和APP名字

--- gen/link/shangxin/movies/R.java
+++ gen/link/shangxin/movies/R.java
@@ -72,11 +72,10 @@ public final class R {
         public static final int welcome=0x7f070003;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int title_activity_result=0x7f050003;
-        public static final int title_activity_welcome=0x7f050004;
+        public static final int title_activity_result=0x7f050002;
+        public static final int title_activity_welcome=0x7f050003;
     }
     public static final class style {
         /** 
