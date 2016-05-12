06e7d697f5c017bdae7ed67ddd2bfe36edd599a6
wanghu <741106631@qq.com>
2016-05-12 09:27:50 +0800

    修改main

--- gen/link/shangxin/movies/R.java
+++ gen/link/shangxin/movies/R.java
@@ -29,30 +29,33 @@ public final class R {
         public static final int ic_launcher=0x7f020004;
         public static final int search1=0x7f020005;
         public static final int timg=0x7f020006;
+        public static final int tm=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080015;
-        public static final int daoyan=0x7f080005;
-        public static final int dianyingming=0x7f080003;
+        public static final int action_settings=0x7f080017;
+        public static final int daoyan=0x7f080007;
+        public static final int dianyingming=0x7f080005;
         public static final int hotList=0x7f080002;
-        public static final int hotName=0x7f08000b;
-        public static final int hotTboxoffice=0x7f08000e;
-        public static final int hotWboxoffice=0x7f08000d;
-        public static final int hotWk=0x7f08000c;
-        public static final int juqing=0x7f080008;
-        public static final int leixing=0x7f080004;
-        public static final int myLayout=0x7f08000a;
-        public static final int rcentDirector=0x7f080012;
-        public static final int rcentPlayData=0x7f080010;
-        public static final int rcentStar=0x7f080011;
-        public static final int rcentStory=0x7f080014;
-        public static final int rcentTvTitle=0x7f08000f;
-        public static final int rcentType=0x7f080013;
+        public static final int hotName=0x7f08000d;
+        public static final int hotTboxoffice=0x7f080010;
+        public static final int hotWboxoffice=0x7f08000f;
+        public static final int hotWk=0x7f08000e;
+        public static final int juqing=0x7f08000a;
+        public static final int leixing=0x7f080006;
+        public static final int myLayout=0x7f08000c;
+        public static final int rcentDirector=0x7f080014;
+        public static final int rcentPlayData=0x7f080012;
+        public static final int rcentStar=0x7f080013;
+        public static final int rcentStory=0x7f080016;
+        public static final int rcentTvTitle=0x7f080011;
+        public static final int rcentType=0x7f080015;
+        public static final int rmButton=0x7f080003;
         public static final int searchBox=0x7f080000;
         public static final int searchButton=0x7f080001;
-        public static final int shangyingshijian=0x7f080007;
-        public static final int tupian=0x7f080009;
-        public static final int zhuyan=0x7f080006;
+        public static final int shangyingshijian=0x7f080009;
+        public static final int tupian=0x7f08000b;
+        public static final int zhuyan=0x7f080008;
+        public static final int zjButton=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
