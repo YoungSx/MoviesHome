9a31492fb3c2bbc6dab90a6519df4cb20c690af5
Shangxin <shangxin@outlook.com>
2016-05-12 14:20:27 +0800

    添加启动页为welcome

--- src/link/shangxin/movies/WelcomeActivity.java
+++ src/link/shangxin/movies/WelcomeActivity.java
@@ -1,9 +1,13 @@
 package link.shangxin.movies;
 
+import link.shangxin.movies.MainActivity;
+import link.shangxin.movies.WelcomeActivity;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,7 +22,20 @@ public class WelcomeActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
 
-		
+		//推迟多久去做什么事
+		new Handler().postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				// 你要做的事
+				
+				//创建一个意图：WelcomeActivity to MainActivity
+				Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
+				
+				startActivity(intent);
+				
+			}
+		}, 3000);
 	}
 
 }
